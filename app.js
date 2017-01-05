eb4ee301b65080b8b776a7665e07a512346cfcea
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-01-05 19:33:26 +0100

    fonctionel mais grave !

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('have fun !');
+
+var fname;
+var lname;
+var joliObj = new Object();
+
+$('input').bind('keyup', function(e) {
+	if (e.which !== 13) {
+		id = $(this).attr('id');
+		input = $(this).val();
+	}
+	else if (e.which == 13){
+		$('input').each(function(){
+			tId = $(this).attr('id');
+			tInput = $(this).val();
+
+			if (tInput.length > 0){
+				joliObj[tId] = tInput;
+				console.log(tInput + " " + input)
+				console.log(joliObj);
+			}
+			else {
+				console.log('rien');
+			}
+		}
+		)
+	}
+});
+
+$('button').click(function(){
+	$('#username').html(joliObj.first_name + " " + joliObj.last_name);
+	console.log(joliObj);
+})
+
